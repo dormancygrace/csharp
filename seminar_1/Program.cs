aa93553c0e33c50d72c4a40c3daebbe49d467490
dgrace <dormancygrace@gmail.com>
2022-08-05 20:48:59 +0300

    first exercise

---
+++ seminar_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse( Console.ReadLine()!);
+
+    if (a > b) {
+        Console.Write("max = ");
+        Console.Write(a);
+    }
+    else {
+        Console.Write("max = ");
+        Console.Write(b);
+    }
