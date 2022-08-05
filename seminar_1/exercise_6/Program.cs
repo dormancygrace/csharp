abf17dbb2097e6f1ac1ee2512db7cf3503362801
dgrace <dormancygrace@gmail.com>
2022-08-05 21:31:03 +0300

    exercise_6, exercise_8

---
+++ seminar_1/exercise_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+    if (a % 2 == 0) {
+        Console.Write("число " + a + " чётное");
+    }
+    else {
+        Console.Write("число " + a + " нечётное");
+    }
---
+++ seminar_1/exercise_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+
+    while (count <= a) {
+        if (count % 2 == 0) {
+            Console.Write(count);
+        }
+        else Console.Write(" , ");
+        count += 1;    
+    }
