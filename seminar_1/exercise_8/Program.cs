103d6dd3eaf125f65ead671dfc088cf73f7a4537
dgrace <dormancygrace@gmail.com>
2022-08-07 14:13:13 +0300

    fix exercise_8

--- seminar_1/exercise_8/Program.cs
+++ seminar_1/exercise_8/Program.cs
@@ -1,12 +1,28 @@
 ﻿Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
 
-int count = 0;
+int count = 1;
 
     while (count <= a) {
-        if (count % 2 == 0) {
-            Console.Write(count);
+        if (count % 2 == 0 && count < a) {
+            Console.Write(count);   
         }
-        else Console.Write(" , ");
-        count += 1;    
+        if (count % 2 == 0 && count == a) {
+            Console.Write(count + ".");
+            count++;
+        }
+        if (a <= 1) {
+            Console.Write("вы ввели cлишком маленькое число");
+        }
+        if (count % 2 != 0 && count == a) {
+            Console.Write(".");
+            count++;
+        }
+        if (count % 2 != 0 && count < a && count > 1) {
+            Console.Write(", ");
+            count++;
+        }
+       
+        else 
+        count++;    
     }
