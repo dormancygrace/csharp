b7adb46ebded25f2daf0d076a50687e9bf88fc59
dgrace <dormancygrace@gmail.com>
2022-08-10 17:19:03 +0300

    +exercise_10

---
+++ seminar_2/exercise_10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int third = 0;
+
+if (number > 100 && number < 999) {
+third = (number / 10) % 10;
+Console.Write(third);
+}
+
+else 
+Console.Write("Вы ввели что-то не то");
