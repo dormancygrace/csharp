92427a014f88a2d173f02f1ee6b104084028d28f
dgrace <dormancygrace@gmail.com>
2022-08-10 16:20:41 +0300

    +exercise_15

---
+++ seminar_2/exercise_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 || day > 7) {
+Console.Write("Вы ввели что-то не то");
+}
+
+if (day >= 1 && day <= 7 ) {
+    if (day == 6 || day == 7) {
+        Console.Write(day + " -> да");
+    }
+    else
+    Console.Write(day + " -> нет");
+}
