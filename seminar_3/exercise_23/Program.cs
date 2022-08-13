e4ef715849c19021a86e76b32f208fd59d2acd8d
dgrace <dormancygrace@gmail.com>
2022-08-13 14:53:56 +0300

    +exercise_23

---
+++ seminar_3/exercise_23/Program.cs
@@ -0,0 +1,33 @@
+﻿void FillArray(int[] result) 
+{
+    int length = result.Length;
+    int index = 0;
+    int count = 1;
+    while (index < length) {
+        result[index] = Convert.ToInt32(Math.Pow(count, 3));
+        index++;
+        count++;
+        
+    }
+}
+void PrintArray(int[] numbers) 
+{
+    int cou = numbers.Length;
+    int position = 0;
+    Console.Write(cou + " -> ");
+    while (position < cou) {
+         if (position < cou - 1) {
+            Console.Write(numbers[position] + ", ");
+            position++;
+        }
+        else if (position < cou) {
+            Console.Write(numbers[position] + ".");
+            position++;
+        }       
+    }
+}
+Console.Write("Введите число: ");
+int baseN = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [baseN];
+    FillArray(array);
+    PrintArray(array);
