5252751369ee752aeeb70fc14737209c316a98b8
dgrace <dormancygrace@gmail.com>
2022-08-15 12:46:13 +0300

    +exercise_19

---
+++ seminar_3/exercise_19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number <= 99999) {
+    int first = number / 10000;
+    int second = (number / 1000) % 10;
+    int fourth = (number % 100) / 10;
+    int fifth = number % 10;
+    if (first == fifth && second == fourth) {
+        Console.Write(number + " -> да" );
+    }
+    
+    else 
+    Console.Write(number + " -> нет"); 
+}
+else 
+Console.Write("Вы ввели что-то не то");
