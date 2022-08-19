b990b9b9c500538b0708ad793ce70ba0a03dd62e
dgrace <dormancygrace@gmail.com>
2022-08-19 22:05:31 +0300

    +exercise_34

---
+++ seminar_5/exercise_34/Program.cs
@@ -0,0 +1,33 @@
+﻿void FillArray(int[] input) 
+{  
+    int index = 0;
+    int length = input.Length;
+    for (; index < length; index++) {
+        input[index] = new Random().Next(100,999);
+
+    } 
+    
+}
+void FindEven(int[] array) {
+    int index = 0;
+    int length = array.Length;
+    
+    for (; index < length; index++ ) {
+        while (array[index] % 2 == 0) {
+            if (index == length - 1) {
+                Console.Write(array[index] + ". ");
+                break;
+            }
+            else {
+                Console.Write(array[index] + ", ");
+                break;
+                }
+        }
+        
+    }
+}
+Console.Write("Введите желаемое число элементов массива: ");
+int index = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [index];
+FillArray(array);
+FindEven(array); 
