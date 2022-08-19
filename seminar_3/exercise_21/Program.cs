442ae29653c76b23a33dcefa107d0a925b637a26
dgrace <dormancygrace@gmail.com>
2022-08-19 19:57:21 +0300

    fix

--- seminar_3/exercise_21/Program.cs
+++ seminar_3/exercise_21/Program.cs
@@ -1,6 +1,7 @@
 ﻿double LengthBetweenPoints (int x1, int y1, int z1, int x2, int y2, int z2) {
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 }
+
 Console.Write("Введите координату точки A по оси x: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату точки A по оси y: ");
