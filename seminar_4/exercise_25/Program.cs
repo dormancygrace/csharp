7753f67e631c73539e8748703d14bcd8733ba424
dgrace <dormancygrace@gmail.com>
2022-08-19 20:39:14 +0300

    +exercise_25

---
+++ seminar_4/exercise_25/Program.cs
@@ -0,0 +1,18 @@
+﻿int Exponentiation (int A, int B) {
+    int result = 0;
+    int i;
+    for (i = B; i == B; i++) {
+        result = Convert.ToInt32(Math.Pow(A, B));
+    }
+return result;
+}
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Exponentiation(A, B));
+
+
