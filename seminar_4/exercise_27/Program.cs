f12b6e9a06ecc8dbd022249ca212ca6db6402d49
dgrace <dormancygrace@gmail.com>
2022-08-19 21:14:50 +0300

    +exercise_27

---
+++ seminar_4/exercise_27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int op = 0;
+
+if (number < 0) {
+    number = number * -1;
+ }
+
+    while (number > 0)
+            {
+ 
+                op = op + number % 10;
+                number = number / 10;
+ 
+            }
+ Console.Write(op);
+
