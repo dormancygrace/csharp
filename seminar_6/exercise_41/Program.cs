39eb3c8f63f3f87e280647e0063500eba3487b6f
dgrace <dormancygrace@gmail.com>
2022-08-27 10:32:27 +0300

    +exercise_41

---
+++ seminar_6/exercise_41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите числа: ");
+string? s = Console.ReadLine();
+
+int[] array = s.Split(',', ' ').Select(n => Convert.ToInt32(n)).ToArray();
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+    if (i == array.Length - 1) {
+                Console.Write(array[i] + " => ");
+    }
+    else Console.Write(array[i] + ", ");
+}
+ 
+Console.WriteLine($"{count}");
