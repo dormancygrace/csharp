d352b08354efe039f68bcd509359abb8984dabc5
dgrace <dormancygrace@gmail.com>
2022-09-03 21:05:36 +0300

    fix exercise_68

--- seminar_9/exercise_68/Program.cs
+++ seminar_9/exercise_68/Program.cs
@@ -9,10 +9,12 @@
                 {
                     return Akkerman(m - 1, 1);
                 } 
+                
                 if (m > 0 && n > 0)
                 {
                     return Akkerman(m - 1, Akkerman(m, n - 1));
                 } 
+                
                 return Akkerman(m,n);
             }
 
@@ -23,9 +25,9 @@ long m = Convert.ToInt32(Console.ReadLine());
 Console.Write($"Введите N: ");
 long n = Convert.ToInt32(Console.ReadLine());
 
-if (m > 0 && n > 0) 
+if (m >= 0 && n >= 0) 
 {
-Console.Write(Akkerman(m,n));
+Console.Write($"m = {m}, n = {n} -> A({m},{n}) = {Akkerman(m,n)}");
 }
 else
 Console.Write($"Одно или оба числа < 0");
