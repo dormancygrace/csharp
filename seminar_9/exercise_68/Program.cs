5a88187f55f89c7664508d983ad9832eccdc5e8b
dgrace <dormancygrace@gmail.com>
2022-09-03 20:56:08 +0300

    +exercise_68

---
+++ seminar_9/exercise_68/Program.cs
@@ -0,0 +1,31 @@
+﻿long Akkerman (long m, long n)
+            {
+                if (m == 0)
+                {
+                    return n + 1;
+                }
+
+                if (m > 0 && n == 0)
+                {
+                    return Akkerman(m - 1, 1);
+                } 
+                if (m > 0 && n > 0)
+                {
+                    return Akkerman(m - 1, Akkerman(m, n - 1));
+                } 
+                return Akkerman(m,n);
+            }
+
+
+Console.Write($"Введите M: ");
+long m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите N: ");
+long n = Convert.ToInt32(Console.ReadLine());
+
+if (m > 0 && n > 0) 
+{
+Console.Write(Akkerman(m,n));
+}
+else
+Console.Write($"Одно или оба числа < 0");
