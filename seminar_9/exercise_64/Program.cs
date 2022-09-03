358b00877fc2dacedd3e79f7f85c8149f4263272
dgrace <dormancygrace@gmail.com>
2022-09-03 19:16:40 +0300

    +exercise_64

---
+++ seminar_9/exercise_64/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write($"Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int diff = n - m;
+
+if (diff < 0)
+{
+    Console.Write($"Натуральных чисел нет");
+}
+
+if (diff == 0)
+{
+    Console.Write($"M = {m}; N = {n}. -> {m}");
+}
+
+
+if (diff > 0)
+{
+    Console.Write($"M = {m}; N = {n}. -> ");
+    for (int i = m; i <= n; i++)
+    {
+        if (i < n)
+        {
+            Console.Write($"{i}, ");
+        }
+        else Console.Write($"{i}");
+        
+    }
+    
+
+
+}
